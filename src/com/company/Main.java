4b432105b7dc128a32f0f772251da66d7a4e0934
cristianabote <cristianamihaelaplesa@gmail.com>
2021-03-11 11:51:30 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Numere pare pana la 1000
+        for(int i=0;i<=1000;i++){
+            if(i%2==0){
+                System.out.println("Numarul "+i+" este par.");
+            }
+
+        }
+    }
+}
